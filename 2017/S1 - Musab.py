9473489854a1328ad55d2a50ede25d4b6b4f9445
Musab1Blaser <musab16000@gmail.com>
2022-02-02 02:15:20 +0500

    beat awab

---
+++ 2017/S1 - Musab.py
@@ -0,0 +1,17 @@
+def inputVal():
+    n=input()
+    swi=input().split()
+    sem=input().split()
+    return n,swi, sem
+
+n,swi,sem= inputVal()
+TSwi=[0]
+TSem=[0]
+lastDay=0
+for i in range(1,int(n)+1):
+    TSwi.append(TSwi[i-1]+int(swi[i-1]))
+    TSem.append(TSem[i-1]+int(sem[i-1]))
+    if TSwi[i]==TSem[i]:
+        lastDay=i
+
+print(lastDay)
