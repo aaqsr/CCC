96447320dfb612b1f07520f5a68439a048d85688
Awab <awab.qureshi@gmail.com>
2022-02-02 02:21:55 +0500

    Fomatting

--- 2017/S1 - Musab.py
+++ 2017/S1 - Musab.py
@@ -1,17 +1,18 @@
 def inputVal():
-    n=input()
-    swi=input().split()
-    sem=input().split()
-    return n,swi, sem
+    n = input()
+    swi = input().split()
+    sem = input().split()
+    return n, swi, sem
 
-n,swi,sem= inputVal()
-TSwi=[0]
-TSem=[0]
-lastDay=0
-for i in range(1,int(n)+1):
-    TSwi.append(TSwi[i-1]+int(swi[i-1]))
+
+n, swi, sem = inputVal()
+TSwi = [0]
+TSem = [0]
+lastDay = 0
+for i in range(1, int(n)+1):
+    TSwi.append(TSwi[i-1] + int(swi[i-1]))
     TSem.append(TSem[i-1]+int(sem[i-1]))
-    if TSwi[i]==TSem[i]:
-        lastDay=i
+    if TSwi[i] == TSem[i]:
+        lastDay = i
 
 print(lastDay)
