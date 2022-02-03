3f502f76d0213aadc857becc81c1013e3cbeb276
Awab <awab.qureshi@gmail.com>
2022-02-03 23:14:59 +0500

    Formatting

--- 2018/S3 - Musab.py
+++ 2018/S3 - Musab.py
@@ -1,121 +1,129 @@
 def takeInput(n):
-    grid=[]
+    grid = []
     for i in range(n):
-        temp=input()
+        temp = input()
         grid.append(temp)
-        if temp.find("S")!=-1:
-            startY=i
-            startX=temp.find("S")
-    return grid,[startY,startX]
+        if temp.find("S") != -1:
+            startY = i
+            startX = temp.find("S")
+    return grid, [startY, startX]
 
-def breadSearch(frontier,fpointer,cost):
-    while fpointer<len(frontier):
-        temp1,temp2=checkAdj(frontier[fpointer], cost[fpointer])
-        if len(temp1)!=0:
+
+def breadSearch(frontier, fpointer, cost):
+    while fpointer < len(frontier):
+        temp1, temp2 = checkAdj(frontier[fpointer], cost[fpointer])
+        if len(temp1) != 0:
             frontier.extend(temp1)
             cost.extend(temp2)
-        fpointer+=1
+        fpointer += 1
+
 
 def checkAdj(coords, currCost):
-    Rlist=[]
-    Clist=[]
-    temp = ValidNode([coords[0]-1,coords[1]])
+    Rlist = []
+    Clist = []
+    temp = ValidNode([coords[0]-1, coords[1]])
     if temp[0]:
         Rlist.append(temp[1])
         Clist.append(currCost+1)
 
-    temp = ValidNode([coords[0]+1,coords[1]])
-    if temp[0]:
-        Rlist.append(temp[1])
-        Clist.append(currCost+1)
-        
-    temp = ValidNode([coords[0],coords[1]-1])
+    temp = ValidNode([coords[0]+1, coords[1]])
     if temp[0]:
         Rlist.append(temp[1])
         Clist.append(currCost+1)
 
-    temp = ValidNode([coords[0],coords[1]+1])
+    temp = ValidNode([coords[0], coords[1]-1])
     if temp[0]:
         Rlist.append(temp[1])
         Clist.append(currCost+1)
-    return Rlist,Clist
+
+    temp = ValidNode([coords[0], coords[1]+1])
+    if temp[0]:
+        Rlist.append(temp[1])
+        Clist.append(currCost+1)
+    return Rlist, Clist
+
 
 def ValidNode(coords):
-    validity=True
+    validity = True
     global allNodes
     global grid
     if coords in allNodes:
-        validity=False
+        validity = False
     else:
         allNodes.append(coords.copy())
-        row=grid[coords[0]]
-        col=getCol(coords[1])
-        temp= grid[coords[0]][coords[1]]
+        row = grid[coords[0]]
+        col = getCol(coords[1])
+        temp = grid[coords[0]][coords[1]]
         if temp == "W":
-            validity=False
+            validity = False
 
-        elif temp in ["U","D","L","R"]:
-            testCoords=coords
-            if temp=="U":
+        elif temp in ["U", "D", "L", "R"]:
+            testCoords = coords
+            if temp == "U":
                 testCoords[0] -= 1
-            elif temp=="D":
+            elif temp == "D":
                 testCoords[0] += 1
-            elif temp=="L":
+            elif temp == "L":
                 testCoords[1] -= 1
-            elif temp=="R":
-                testCoords[1] +=1
-            validity, coords=ValidNode(testCoords)
+            elif temp == "R":
+                testCoords[1] += 1
+            validity, coords = ValidNode(testCoords)
 
         elif "C" in row or "C" in col:
-            validity = checkCam(coords,col,row)
+            validity = checkCam(coords, col, row)
     return [validity, coords]
 
-def checkCam(coords,col,row):
-    left=row[:coords[1]][::-1]
-    right=row[coords[1]+1:]
-    up=col[:coords[0]][::-1]
-    down=col[coords[0]+1:]
-    rflag= checkDir(right) and checkDir(left) and checkDir(up) and checkDir(down)
+
+def checkCam(coords, col, row):
+    left = row[:coords[1]][::-1]
+    right = row[coords[1]+1:]
+    up = col[:coords[0]][::-1]
+    down = col[coords[0]+1:]
+    rflag = checkDir(right) and checkDir(
+        left) and checkDir(up) and checkDir(down)
     return rflag
+
 
 def checkDir(dir):
     for i in dir:
-        if i=="C":
-            rflag=False
+        if i == "C":
+            rflag = False
             break
-        if i=="W":
-            rflag=True
+        if i == "W":
+            rflag = True
             break
     return rflag
 
+
 def getCol(colNum):
-    col=[]
+    col = []
     global n
     global grid
     for i in range(n):
         col.append(grid[i][colNum])
     return col
 
-def Display(n,m):
+
+def Display(n, m):
     global frontier
     global cost
     for r in range(n):
         for c in range(m):
-            coords=[r,c]
-            if grid[r][c]==".":
+            coords = [r, c]
+            if grid[r][c] == ".":
                 if coords in frontier:
                     print(cost[frontier.index(coords)])
                 else:
                     print(-1)
 
-temp=input().split()
-n=int(temp[0])
-m=int(temp[1])
-grid, startCoords=takeInput(n)
-frontier=[startCoords]
-fpointer=0
-cost=[0]
-allNodes=[frontier[0]]
-breadSearch(frontier,fpointer,cost)
-Display(n,m)
 
+temp = input().split()
+n = int(temp[0])
+m = int(temp[1])
+grid, startCoords = takeInput(n)
+frontier = [startCoords]
+fpointer = 0
+cost = [0]
+allNodes = [frontier[0]]
+breadSearch(frontier, fpointer, cost)
+Display(n, m)
