dd0dd4063cd626ff984f50766f5572ae45416a3e
Musab1Blaser <musab16000@gmail.com>
2022-02-05 02:58:58 +0500

    ymca

--- 2015/S2 - Musab.py
+++ 2015/S2 - Musab.py
@@ -1,26 +1,31 @@
+import itertools
+
+
 def giveJersey(num, size):
-    shirts=["S","M","L"]
+    shirts = ["S", "M", "L"]
     global jerseys
     if jerseys[num] in shirts[shirts.index(size):]:
-        jerseys[num]="X"
+        jerseys[num] = "X"
         return True
     else:
         return False
 
+
 def takeInp():
-    j=int(input())
-    a=int(input())
+    j = int(input())
+    a = int(input())
     global jerseys
-    satisfied=0
+    satisfied = 0
 
     for i in range(j):
-        jerseys[i+1]=input()
+        jerseys[i+1] = input()
 
     for i in range(a):
-        size,num = input().split()
-        if giveJersey(int(num),size):
-            satisfied+=1
+        size, num = input().split()
+        if giveJersey(int(num), size):
+            satisfied += 1
     print(satisfied)
 
-jerseys={}
+
+jerseys = {}
 takeInp()
