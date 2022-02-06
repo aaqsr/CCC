ee2b65c9ffa29f658bd1669ddc4283c67a375aa5
Musab1Blaser <musab16000@gmail.com>
2022-02-06 21:48:01 +0500

    neat

--- 2020/S2 - Musab.py
+++ 2020/S2 - Musab.py
@@ -1,39 +1,34 @@
-from logging.config import valid_ident
-
-
-def takeInp(grid,m,n):
+def takeInp(grid, m, n):
     for r in range(m):
-        temp=[int(x) for x in input().split()]
+        temp = [int(x) for x in input().split()]
         for c in range(n):
-            grid[temp[c]]=grid.get(temp[c],[])+[[r,c]]
-    
-def recPath(r,c):
+            grid[temp[c]] = grid.get(temp[c], [])+[[r, c]]
+
+
+def recPath(r, c):
     global frontier
-    val=(r+1)*(c+1)
+    val = (r+1)*(c+1)
     global grid
     global Validity
-    if val==1:
-        Validity=True
+    if val == 1:
+        Validity = True
     elif val in grid:
         for cord in grid[val]:
             if not(cord in frontier):
                 frontier.append(cord)
 
-m=int(input())
-n=int(input())
-frontier=[[m-1,n-1]]
-fpointer=0
-grid={}
-takeInp(grid,m,n)
-# if recPath(m-1,n-1,[]):
-#     print("yes")
-# else:
-#     print("no")
 
-Validity=False
-while fpointer<len(frontier) and Validity == False:
-    recPath(frontier[fpointer][0],frontier[fpointer][1])
-    fpointer+=1
+m = int(input())
+n = int(input())
+frontier = [[m-1, n-1]]
+fpointer = 0
+grid = {}
+takeInp(grid, m, n)
+
+Validity = False
+while fpointer < len(frontier) and Validity == False:
+    recPath(frontier[fpointer][0], frontier[fpointer][1])
+    fpointer += 1
 
 if Validity:
     print("yes")
---
+++ 2020/test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    printf("hello");
+    return 0;
+}
